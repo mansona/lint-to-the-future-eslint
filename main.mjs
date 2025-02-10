0b252405cb0ad4126da5fcc94c668fd9d36bb560
Chris Manson <chris@manson.ie>
2025-02-10 11:45:49 +0000

    support filter ignore

--- main.mjs
+++ main.mjs
@@ -1,3 +1,5 @@
 export { default as ignoreAll } from './lib/ignore-all.js';
 export { default as list } from './lib/list.js';
 export { default as remove } from './lib/remove.js';
+
+export const capabilities = ['filter-ignore'];
--- test/ignore.test.js
+++ test/ignore.test.js
@@ -1,13 +1,10 @@
-import { expect } from 'chai';
 import { Project } from 'fixturify-project';
-import { join } from 'path';
-import { readFileSync } from 'fs';
 
-import { describe, it } from 'vitest';
+import { describe, it, expect } from 'vitest';
 
 import { ignoreAll } from '../main.mjs';
 
-async function ignoreTestFile(input, files) {
+async function ignoreTestFile(input, files, options) {
   const project = new Project({
     files: {
       '.eslintrc.json': '{"extends": "eslint:recommended"}',
@@ -30,22 +27,23 @@ module.exports = [
   project.linkDevDependency('eslint', { baseDir: process.cwd() });
   project.linkDevDependency('@eslint/js', { baseDir: process.cwd() });
   await project.write();
-  await ignoreAll(project.baseDir);
+  await ignoreAll(options ?? {}, project.baseDir);
 
-  return readFileSync(join(project.baseDir, 'index.js'), 'utf-8');
+  project.readSync(project.baseDir);
+  return project.files;
 }
 
 describe('ignore function', function () {
   it('should not crash with ^ in eslint dependency', async function () {
-    expect(await ignoreTestFile(`debugger`)).to
+    expect((await ignoreTestFile(`debugger`))['index.js']).to
       .equal(`/* eslint-disable no-debugger */
 debugger`);
   });
 
   it('should handle files with invalid `// eslint-disable` comments at the top', async function () {
     expect(
-      await ignoreTestFile(`// eslint-disable no-debugger
-debugger`),
+      (await ignoreTestFile(`// eslint-disable no-debugger
+debugger`))['index.js'],
     ).to.equal(`/* eslint-disable no-debugger */
 // eslint-disable no-debugger
 debugger`);
@@ -53,9 +51,9 @@ debugger`);
 
   it('should add to existing `/* eslint-disable` comments', async function () {
     expect(
-      await ignoreTestFile(`/* eslint-disable no-console, no-undef */
+      (await ignoreTestFile(`/* eslint-disable no-console, no-undef */
 debugger
-console.log('test')`),
+console.log('test')`))['index.js'],
     ).to.equal(`/* eslint-disable no-console, no-debugger, no-undef */
 debugger
 console.log('test')`);
@@ -63,9 +61,9 @@ console.log('test')`);
 
   it('handles `// eslint-disable-next-line` at the top of the file correctly', async function () {
     expect(
-      await ignoreTestFile(`/* eslint-disable-next-line no-debugger */
+      (await ignoreTestFile(`/* eslint-disable-next-line no-debugger */
 debugger
-console.log('test')`),
+console.log('test')`))['index.js'],
     ).to.equal(`/* eslint-disable no-undef */
 /* eslint-disable-next-line no-debugger */
 debugger
@@ -74,15 +72,15 @@ console.log('test')`);
 
   it('handles rules with slashes in the name', async function () {
     expect(
-      await ignoreTestFile(`/* eslint-disable ember/no-observers */
-debugger`),
+      (await ignoreTestFile(`/* eslint-disable ember/no-observers */
+debugger`))['index.js'],
     ).to.equal(`/* eslint-disable ember/no-observers, no-debugger */
 debugger`);
   });
 
   it('does not add ignores for warnings reported by eslint', async function () {
     expect(
-      await ignoreTestFile(
+      (await ignoreTestFile(
         `debugger
 
 if (10 === 'false') {
@@ -99,7 +97,7 @@ if (10 === 'false') {
               { rules: { "no-constant-condition": "warn" }}
           ];`,
         },
-      ),
+      ))['index.js'],
     ).to.equal(`/* eslint-disable no-debugger */
 debugger
 
@@ -110,18 +108,45 @@ if (10 === 'false') {
 
   it('does the right thing with hash-bang/shebang files', async function () {
     expect(
-      await ignoreTestFile(`#!/usr/bin/env node
-debugger`),
+      (await ignoreTestFile(`#!/usr/bin/env node
+debugger`))['index.js'],
     ).to.equal(`#!/usr/bin/env node
 /* eslint-disable no-debugger */
 debugger`);
 
     expect(
-      await ignoreTestFile(`#!/usr/bin/env node
+      (await ignoreTestFile(`#!/usr/bin/env node
 /* eslint-disable some-other-lint */
-debugger`),
+debugger`))['index.js'],
     ).to.equal(`#!/usr/bin/env node
 /* eslint-disable no-debugger, some-other-lint */
 debugger`);
   });
+
+  it('supports ignore filter correctly', async function() {
+    const files = await ignoreTestFile(``, {
+      'ignore-me.js': 'debugger',
+      'update-me.js': 'debugger',
+      'update-me-too.js': 'debugger',
+    }, {
+      filter: 'update*.js'
+    });
+
+    expect(files).toMatchInlineSnapshot(`
+      {
+        ".eslintrc.json": "{"extends": "eslint:recommended"}",
+        "eslint.config.js": "const js = require("@eslint/js");
+
+      module.exports = [
+          js.configs.recommended,
+      ];",
+        "ignore-me.js": "debugger",
+        "index.js": "",
+        "update-me-too.js": "/* eslint-disable no-debugger */
+      debugger",
+        "update-me.js": "/* eslint-disable no-debugger */
+      debugger",
+      }
+    `)
+  })
 });
